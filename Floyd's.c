86ecc13f39120ba6dfd26be763ea89061fcbc096
YashaswiniGA <117418945+YashaswiniGA@users.noreply.github.com>
2023-07-25 15:52:34 +0530

    Add files via upload

---
+++ Floyd's.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<conio.h>
+int c[10][10],d[10][10],n;
+void floyd()
+{
+    int i,j,k;
+    for(i=1;i<=n;i++)
+    {
+
+        for(j=1;j<=n;j++)
+        {
+            d[i][j]=c[i][j];
+        }
+    }
+    for(k=1;k<=n;k++)
+    {
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n;j++)
+            {
+                d[i][j]=(d[i][j]<d[i][k]+d[k][j])?d[i][j]:(d[i][k]+d[k][j]);
+            }
+        }
+
+    }
+}
+void main()
+{
+    int i,j;
+    printf("Enter the number of vertices:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            scanf("%d",&c[i][j]);
+        }
+    }
+    floyd();
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            printf("%d\t",d[i][j]);
+        }
+        printf("\n");
+    }
+}
